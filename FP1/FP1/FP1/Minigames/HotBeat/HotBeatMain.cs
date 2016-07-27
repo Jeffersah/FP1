e570dd8c7fe6b1b0c155661e6c209d5b7f2a8d82
Ciferoni <chrisman311@comcast.net>
2016-07-27 14:49:10 -0400

    Beginning HotBeat.wav
    Created main class

---
+++ FP1/FP1/FP1/Minigames/HotBeat/HotBeatMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using NCodeRiddian;
+using DataLoader;
+
+namespace FP1.Minigames.HotBeat
+{
+    class HotBeatMain : Minigame
+    {
+
+        public override void Load(ContentManager cm)
+        {
+
+        }
+
+        public override void Start(Player[] InGame)
+        {
+
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gt, Screens.MinigameScreen parentScreen)
+        {
+            
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gt, Microsoft.Xna.Framework.Graphics.SpriteBatch sb)
+        {
+            
+        }
+
+        public override void RunAI(Player p, Difficulty difficulty)
+        {
+            
+        }
+
+    }
+}
